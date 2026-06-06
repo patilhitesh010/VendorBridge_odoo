32d804f9ce24f94f1faadee497d556e8576a8fb8
Pradip Lalpura <ptlalpura08@gmail.com.com>
2026-06-06 16:00:40 +0530

    fix: resolve SQLITE_CANTOPEN error by updating database path to project root

--- db/init.js
+++ db/init.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 const bcrypt = require('bcrypt');
 const path = require('path');
 
-const dbPath = path.join(__dirname, '../data/vendorbridge.db');
+const dbPath = path.join(__dirname, '../vendorbridge.db');
 const db = new sqlite3.Database(dbPath);
 
 async function initDb() {
